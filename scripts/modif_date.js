be777935db3f4f6fc2fa801c89750f850f3e0a56
bejaranopol <104479619+bejaranopol@users.noreply.github.com>
2023-01-12 19:24:26 -0500

    changes according to feedback of  Mr Matias Villar

---
+++ scripts/modif_date.js
@@ -0,0 +1,5 @@
+let text = document.lastModified;       
+document.getElementById('last_modified').innerHTML = text;
+
+let year = new Date().getFullYear();
+document.getElementById('c_year').innerHTML = year;
