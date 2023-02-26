724374bfa17ceee684db07fd17d0d3848250a6e4
bejaranopol <104479619+bejaranopol@users.noreply.github.com>
2023-02-25 23:08:38 -0500

    w08

--- chamber/scripts/date_mod.js
+++ chamber/scripts/date_mod.js
@@ -7,3 +7,10 @@ let text1 = document.lastModified;
 document.getElementById('last_modified1').innerHTML = text;
 let year1 = new Date().getFullYear();
 document.getElementById('c_year1').innerHTML = year;
+/*
+const userdate = new Date();
+document.getElementById("user_date_mod").nodeValue = userdate.toDateString();
+var hours = user_form_load.getHours();
+var min = user_form_load.getMinutes();
+var seconds = user_form_load.getSeconds();
+document.getElementById('user_form_loaded').innerHTML="*The form was loaded at: "+x+hours+min+seconds;*/
