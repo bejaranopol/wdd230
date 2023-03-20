e033fd00f669e78be6e206bf8ff7a733567237c9
bejaranopol <104479619+bejaranopol@users.noreply.github.com>
2023-03-19 23:13:21 -0500

    w11

--- chamber/scripts/date_mod.js
+++ chamber/scripts/date_mod.js
@@ -1,12 +1,24 @@
-let text = document.lastModified;       
-document.getElementById('last_modified').innerHTML = text;
-let year = new Date().getFullYear();
+const d=new Date();
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let month = months[d.getMonth()];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[d.getDay()];
+
+// Displaying the current date and the last date of modification ↓↓↓
+let year ="   "+day+", "+new Date().getDate()+" "+month+"  "+new Date().getFullYear()+" ; ";
 document.getElementById('c_year').innerHTML = year;
 
+let text = document.lastModified;       
+document.getElementById('last_modified').innerHTML = text;
+
+
+
+
+/**/
 let text1 = document.lastModified;       
 document.getElementById('last_modified1').innerHTML = text;
 let year1 = new Date().getFullYear();
-document.getElementById('c_year1').innerHTML = year;
+document.getElementById('c_year1').innerHTML = year1;
 /*
 const userdate = new Date();
 document.getElementById("user_date_mod").nodeValue = userdate.toDateString();
