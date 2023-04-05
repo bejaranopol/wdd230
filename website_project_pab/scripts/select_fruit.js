1acb6bc0f8e6c22062a9507fc47948a4edbbf51f
bejaranopol <104479619+bejaranopol@users.noreply.github.com>
2023-04-05 10:51:00 -0500

    s

---
+++ website_project_pab/scripts/select_fruit.js
@@ -0,0 +1,18 @@
+const url="https://brotherblazzard.github.io/canvas-content/fruit.json";
+//const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+async function getFruitData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+   // console.table(data.name);  
+   // console.table(data);
+   console.log(jsonData["name"]);
+    
+}
+
+    // note that we reference the prophet array of the data object given the structure of the json file
+   // displayFruits(data.name);
+    //↑↑ AQUI MOSTRAMOS LA DATA DE LA DATA DEL URL
+  }   getFruitData(url);
+
+  
